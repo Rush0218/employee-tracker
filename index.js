e75c6f463b9f473826125b5097e0888ba775d6cb
solera2 <rushtre1@gmail.com>
2022-01-12 18:38:09 -0500

    created function to initiate inquirer

--- index.js
+++ index.js
@@ -2,6 +2,55 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const console = require('console.table');
-const connection = require('config/connection.js');
 
+
+
+function initiate() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        "View All Employees",
+        "View All Employee's By Role",
+        "View all Emplyees By Deparment",
+        "Update Employee",
+        "Add Employee",
+        "Add Role",
+        "Add Department"
+      ]
+    }
+  ]).then(function (input) {
+    switch (input.choice) {
+      case "View All Employees":
+        viewAllEmployees();
+        break;
+
+      case "View All Employee's By Role":
+        viewAllRoles();
+        break;
+
+      case "View all Emplyees By Deparment":
+        viewAllDepts();
+        break;
+
+      case "Update Employee":
+        updateEmployee();
+        break;
+
+      case "Add Employee":
+        addEmployee();
+        break;
+
+      case "Add Role":
+        addRole();
+        break;
+
+      case "Add Department":
+        addDept();
+        break;
+    }
+  })
+};
 
