57b8615a6196a4bd7fb126427711b41adc5fb4b8
solera2 <rushtre1@gmail.com>
2022-01-12 18:24:29 -0500

    created connection.js to connect to mysql server that will reference a env file for credentials

---
+++ index.js
@@ -0,0 +1,7 @@
+//import modules
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const console = require('console.table');
+const connection = require('config/connection.js');
+
+
