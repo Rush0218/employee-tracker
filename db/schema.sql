a98ed25d5b40194cc78df8835e06b60b4d66a0a0
solera2 <rushtre1@gmail.com>
2022-01-12 18:02:31 -0500

    created schema db

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,55 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS employee;
+--Create Database--
+DROP DATABASE IF EXISTS company; 
+CREATE DATABASE company; 
+USE company; 
  
 
+--Create Tables--
 
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INTEGER NOT NULL,
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL,
-  manager_id INTEGER NOT NULL,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL, 
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES 
+  id INTEGER  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) ,
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES role(id), 
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+
+--Department Seeds--
+INSERT INTO department (name) VALUE ("Sales");
+INSERT INTO department (name) VALUE ("Tech");
+INSERT INTO department (name) VALUE ("Finance");
+INSERT INTO department (name) VALUE ("Legal");
+
+--Role Seeds--
+INSERT INTO role (title, salary, department_id) VALUE ("Senior Software Engineer", 175000, 2);
+INSERT INTO role (title, salary, department_id) VALUE ("Legal Team Lead", 200000, 4);
+INSERT INTO role (title, salary, department_id) VALUE ("Accountant", 95000, 3);
+INSERT INTO role (title, salary, department_id) VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id) VALUE ("Sales Rep", 80000, 1);
+INSERT INTO role (title, salary, department_id) VALUE ("Software Engineer", 125000, 2);
+INSERT INTO role (title, salary, department_id) VALUE ("Lawyer", 190000, 4);
+
+--Employee Seeds--
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUE ("Tre", "Rush", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUE ("Natasha", "Romanoff", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUE ("Stephen","Strange",null ,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUE ("Tony", "Stark", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUE ("John", "Wick", 4, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUE ("Peter", "Parker", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUE ("Jinwoo", "Sung", 2, 7);
