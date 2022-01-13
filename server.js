dd33d74f4b00b723108b3907724b5f8df09504f4
solera2 <rushtre1@gmail.com>
2022-01-12 22:01:15 -0500

    created fucntions for viewing different tables

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const consoleTable = require('console.table');
 
 require('dotenv').config();
 
+var roleArr = [];
+var managerArr = [];
+
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -71,14 +74,45 @@ function initiatePrompt() {
   })
 };
 
+
+
 function viewAllEmployees() {
   connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
     function (err, res) {
-      if (err) throw err
+      if (err) {
+        throw err
+      }
       console.table(res)
       initiatePrompt();
     })
 };
+
+function viewAllRoles() {
+  connection.query('SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;',
+    function (err, res) {
+      if (err) {
+        throw err;
+      }
+      console.table(res);
+      initiatePrompt();
+    })
+};
+
+function viewAllDepts() {
+  connection.query('SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;',
+    function (err, res) {
+      if (err) {
+        throw err;
+      }
+      console.table(res);
+      initiatePrompt();
+    })
+};
+
+
+
+
+
 
 
 
